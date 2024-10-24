8c02133f91060aa3cd812d81d209d3a702e5528d
prisca <priscasum07@gmail.com>
2024-10-25 02:46:39 +0800

    test

--- all_users.java
+++ all_users.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.HashMap;
 
 public class all_users {
-	//testing commit and push
+	//testing github
 	private String hospitalId;
     private String password;
     private String role;
