348eaa46cf39e64d3f523069a8dab1fba8da63ff
prisca <priscasum07@gmail.com>
2024-10-25 01:28:02 +0800

    testing eclipse to github

--- all_users.java
+++ all_users.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.HashMap;
 
 public class all_users {
-	//testing commit and push from eclipse
+	//testing commit and push
 	private String hospitalId;
     private String password;
     private String role;
