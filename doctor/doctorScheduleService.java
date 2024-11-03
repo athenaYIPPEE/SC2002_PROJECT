5dc5c5ad586d40a7c73c26cde82330388457a047
ziyinginc <164986918+ziyinginc@users.noreply.github.com>
2024-11-03 23:49:01 +0800

    Update doctorScheduleService.java

--- doctor/doctorScheduleService.java
+++ doctor/doctorScheduleService.java
@@ -10,22 +10,23 @@ public class doctorScheduleService {
 	
 	public void viewPersonalScehdule() {
 		System.out.println("Upcoming Appointments:");
-        if (appointments.isEmpty()) {
-            System.out.println("No upcoming appointments.");
-        } else {
-            for (Appointment appointment : appointments) {
-                System.out.println(appointment);  // Display each appointment
-            }
-        }
+        	if (appointments.isEmpty()) {
+            		System.out.println("No upcoming appointments.");
+        	} else {
+            		for (Appointment appointment : appointments) {
+                	System.out.println(appointment);  // Display each appointment
+            		}
+        	}
 	}
 	
 	public void setAvailability() {
 		System.out.println("Available Slots: ");
-        if (slots.isEmpty()) {System.out.println("No available slots.");
-        else {
-        	for (String avail: slots) {
-        		System.out.println(slots);
+        	if (slots.isEmpty()) System.out.println("No available slots.");
+        	else {
+        		for (String avail: slots) {
+        			System.out.println(slots);
+        		}
         	}
-        }
 	
+	}
 }
