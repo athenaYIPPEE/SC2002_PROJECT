31250e4f832ce2c0036b66b389e2956943305cdb
ziyinginc <164986918+ziyinginc@users.noreply.github.com>
2024-11-04 00:01:41 +0800

    Add files via upload

---
+++ medication/Medication.java
@@ -0,0 +1,17 @@
+package labproject;
+
+public enum Medication {
+	ASPIRIN("Aspirin"),
+    IBUPROFEN("Ibuprofen"),
+    PARACETAMOL("Paracetamol");
+	
+	private String name;
+
+    Medication(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ medication/MedicationStock.java
@@ -0,0 +1,41 @@
+package labproject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MedicationStock {
+
+	private Map<Medication, Integer> stock;
+	
+	public MedicationStock() {
+        this.stock = new HashMap<>();
+    }
+	
+	public void addStock(Medication medication, int amount) {
+        if (stock.containsKey(medication)) {
+            stock.put(medication, stock.get(medication) + amount);
+            System.out.println("Added " + amount + " units to " + medication.getName() + ". Current stock: " + stock.get(medication));
+        } else {
+            System.out.println("Medication " + medication.getName() + " not found in stock.");
+        }
+    }
+	
+	public void removeStock(Medication medication, int amount) {
+        if (stock.containsKey(medication)) {
+            int currentStock = stock.get(medication);
+            if (amount <= currentStock) {
+                stock.put(medication, currentStock - amount);
+                System.out.println("Removed " + amount + " units from " + medication.getName() + ". Current stock: " + (currentStock - amount));
+            } else {
+                System.out.println("Insufficient stock. Current stock: " + currentStock);
+            }
+        } else {
+            System.out.println("Medication " + medication.getName() + " not found in stock.");
+        }
+    }
+	
+	public int getStockLevel(Medication medication) {
+        return stock.getOrDefault(medication, 0); // Returns 0 if not found
+    }
+	
+}
