f2e9995c756267f57a3de1e8eca173b7a5364c61
Afzaana512 <167503089+Afzaana512@users.noreply.github.com>
2024-11-07 18:44:38 +0800

    Update Pharmacist.java

--- project/Pharmacist.java
+++ project/Pharmacist.java
@@ -1,15 +1,50 @@
 package project;
 
-import java.util.List;
 
-public class Pharmacist extends AllUsers {
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Pharmacist extends All_users {
     private AppointmentManager appointmentManager;
-    private InventoryMonitor inventoryManager;
+    private InventoryManager inventoryManager;
 
     public Pharmacist(String hospitalId, String password, List<Medication> inventory) {
         super(hospitalId, password, "Pharmacist");
         this.appointmentManager = new AppointmentManager();
-        this.inventoryManager = new InventoryMonitor(inventory);
+        this.inventoryManager = new InventoryManager(inventory);
+    }
+    
+    public void DisplayMenu()
+    {
+    	
+    	System.out.print("Select an Appointment");
+    	Scanner sc = new Scanner(System.in);
+    	int choice = sc.nextInt();
+    	
+    	
+    	
+    System.out.println("Menu: \n" 
+    		  
+    		    + "1: View Appointment Outcome Record \n" 
+    		    + "2: Update Prescription Status \n" 
+    		    + "3: View Medication Inventory \n" 
+    		    + "4: Submit Replenishment Request \n"
+    		    + "5: Logout \n"); 
+        
+    	Scanner sc = new Scanner(System.in);
+    	int option = sc.nextInt();
+    	
+    	switch(option)
+    	{
+    	case 1 -> viewAppointmentOutcome(Appointment appointment);
+    	case 2 -> updatePrescriptionStatus(Appointment appointment, String medicationName);
+    	case 3 -> viewInventory();
+    	case 4 -> submitReplenishmentRequest(String medicationName);
+    	case 5 -> logout();
+    	}
+    			
+    	
     }
 
     public void viewAppointmentOutcome(Appointment appointment) {
