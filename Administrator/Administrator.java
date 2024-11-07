77eeb291008417a6223111057da508f211b82331
Afzaana512 <167503089+Afzaana512@users.noreply.github.com>
2024-11-07 18:19:10 +0800

    Update Administrator.java

--- Administrator/Administrator.java
+++ Administrator/Administrator.java
@@ -1,6 +1,7 @@
 package project;
 
-import java.util.List;
+
+import java.util.*;
 
 public class Administrator extends AllUsers {
     private StaffManager staffManager;
@@ -11,13 +12,42 @@ public class Administrator extends AllUsers {
         this.staffManager = new StaffManager();
         this.inventoryManager = new InventoryManager(inventory);
     }
-
-    public void addStaff(AllUsers staff) {
-        staffManager.addStaff(staff);
+    
+    public void displayMenu()
+    {
+    	
+    	System.out.println("Menu: \n" 
+    		    + "1: Add Staff \n" 
+    		    + "2: Remove Staff \n" 
+    		    + "3: View Appointments details \n" 
+    		    + "4: View and Manage Medication Inventory  \n" 
+    		    + "5: Approve Replenishment Requests \n" 
+    		    + "6: Logout \n"); 
+    }
+    
+    Scanner sc = new Scanner(System.in);
+    int option = sc.nextInt();
+    switch(option)
+    {
+    case 1 : 
+    	addStaff();
+    	break;
+    case 2 -> removeStaff();
+    case 3 -> viewAppointments(List<Appointment> appointment);
+    case 4 -> Pharmacist.manageInventory(String medicationName, int newStock);
+    case 5 -> approveReplenishmentRequest(String medicationName);
+    case 6 -> logout();
     }
 
-    public void removeStaff(String hospitalId) {
-        staffManager.removeStaff(hospitalId);
+    public void addStaff() {
+        staffManager.addStaff();
+    }
+
+    
+    public void removeStaff() {
+    	staffManager.removeStaff();
+        
+        
     }
 
     public void viewAppointments(List<Appointment> appointments) {
